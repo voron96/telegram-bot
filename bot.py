ac77b7db5d5c4409c2e4bf58ac87b89c842d7b60
voron96 <voro9n6@gmail.com>
2026-07-14 12:44:29 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -72,8 +72,9 @@ GOOGLE_MAPS_RE = re.compile(
 async def main_moderation(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     if not update.effective_message or update.effective_chat.id != CHAT_ID:
-        return
-        if update.edited_message or update.edited_channel_post:
+    return
+
+if update.edited_message or update.edited_channel_post:
     return
 
    user = update.effective_user
