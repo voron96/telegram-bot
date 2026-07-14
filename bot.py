a095072ccb92dfe41080dd5aedff4f1d8b7f1c46
voron96 <voro9n6@gmail.com>
2026-07-14 12:27:21 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -74,23 +74,30 @@ async def main_moderation(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if not update.effective_message or update.effective_chat.id != CHAT_ID:
         return
 
-    user = update.effective_user
-    msg = update.effective_message
-    text = msg.text or ""
+   user = update.effective_user
+msg = update.effective_message
+text = msg.text or ""
 
-     # ----- ПОВІДОМЛЕННЯ З ОФІЦІЙНОГО КАНАЛУ -----
-    if msg.sender_chat and msg.sender_chat.id == CHANNEL_ID:
+# ----- АВТОМАТИЧНО ВІДКРІПИТИ ПОВІДОМЛЕННЯ -----
+if msg.pinned_message:
+    try:
+        await context.bot.unpin_chat_message(CHAT_ID)
+    except:
+        pass
 
-        await context.bot.send_message(
-            chat_id=CHAT_ID,
-            text='⬆️ <a href="https://t.me/robota_kiev_workk"><b>Повідомлення з КАНАЛУ ↗️</b></a>',
-            parse_mode="HTML",
-            disable_notification=True,
-            reply_to_message_id=msg.message_id,
-            allow_sending_without_reply=True
-        )
+# ----- ПОВІДОМЛЕННЯ З ОФІЦІЙНОГО КАНАЛУ -----
+if msg.sender_chat and msg.sender_chat.id == CHANNEL_ID:
 
-        return
+    await context.bot.send_message(
+        chat_id=CHAT_ID,
+        text='⬆️ <a href="https://t.me/robota_kiev_workk"><b>Повідомлення з КАНАЛУ ↗️</b></a>',
+        parse_mode="HTML",
+        disable_notification=True,
+        reply_to_message_id=msg.message_id,
+        allow_sending_without_reply=True
+    )
+
+    return
 
     # ----- SYSTEM JOIN / LEFT -----
     if msg.new_chat_members or msg.left_chat_member:
